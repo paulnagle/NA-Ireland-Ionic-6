92a45ab402ff61e2c09ca4b4e315155805ef16f2
Paul Nagle <paulnagle@gmail.com>
2019-06-27 09:38:40 +0100

    Implement translation service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { Storage } from '@ionic/storage';
+import { TranslateService } from '@ngx-translate/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -25,9 +28,20 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private translate: TranslateService,
+    private storage: Storage
   ) {
     this.initializeApp();
+    this.translate.setDefaultLang('en');
+    storage.get('language').then((value) => {
+      if (value) {
+        this.translate.use(value);
+      } else {
+        this.translate.use('en');
+        storage.set('language', 'en');
+      }
+    });
   }
 
   initializeApp() {
