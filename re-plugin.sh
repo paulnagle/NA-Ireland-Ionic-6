2e65a3ba959fc68b703edd6e6e36f62a431f34f0
Paul Nagle <paulnagle@gmail.com>
2019-07-08 21:53:56 +0100

    First pass at Map Search page

--- re-plugin.sh
+++ re-plugin.sh
@@ -4,6 +4,7 @@ rm -rf www
 
 ionic cordova platform rm ios
 ionic cordova platform rm android
+ionic cordova platform rm browser
 
 ionic cordova plugin rm cordova-plugin-inappbrowser
 ionic cordova plugin rm cordova-plugin-insomnia
@@ -14,16 +15,16 @@ ionic cordova plugin rm cordova-plugin-googlemaps
 
 rm -rf platform/*
 
-ionic cordova platform add ios
-ionic cordova platform add android
-ionic cordova platform add browser
+ionic cordova platform add ios@latest
+ionic cordova platform add android@latest
+ionic cordova platform add browser@latest
 
 ionic cordova plugin add cordova-plugin-inappbrowser
 ionic cordova plugin add cordova-plugin-insomnia
 ionic cordova plugin add cordova-plugin-splashscreen
 ionic cordova plugin add cordova-plugin-statusbar
 ionic cordova plugin add cordova-plugin-whitelist
-ionic cordova plugin add cordova-plugin-googlemaps@latest
+ionic cordova plugin add cordova-plugin-googlemaps@latest  --variable API_KEY_FOR_ANDROID="AIzaSyDwPqrrBYFrtCIJ0IUL81tiIVwYJqUMKCw" --variable API_KEY_FOR_IOS="AIzaSyCeHYVhVc8bf3siRXIyU5dOaUfctEjzGlA"
 
 ionic cordova prepare ios --prod
 ionic cordova prepare android --prod
