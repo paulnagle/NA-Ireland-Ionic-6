1659dd0c7856164424fb2c49bf0c862ae8050213
Paul Nagle <paulnagle@gmail.com>
2019-09-01 20:12:40 +0100

    cleantime calc fix

--- src/app/datetime/datetime.page.ts
+++ src/app/datetime/datetime.page.ts
@@ -56,19 +56,31 @@ export class DatetimePage implements OnInit {
     this.cleanTimeDate = new Date(cleanDateInMilliseconds);
     this.cleanDay = this.cleanTimeDate.getDate();
     this.cleanMonth = this.cleanTimeDate.getMonth();
-    this.cleanYear = this.cleanTimeDate.getYear();
+    this.cleanYear = this.cleanTimeDate.getFullYear();
 
     this.cleanTimeInMilliseconds = (this.todayInMilliseconds - cleanDateInMilliseconds) + oneDay; // ??
     this.cleanTimeInDays = Math.floor(this.cleanTimeInMilliseconds / oneDay);
     this.cleanTimeInWeeks = Math.floor(this.cleanTimeInMilliseconds / oneWeek);
     this.cleanTimeInYears = Math.floor(this.cleanTimeInMilliseconds / oneYear);
 
-    this.cleanTimeTag();
+    if (this.cleanTimeInDays !== 0) {
+      this.cleanTimeTag();
+    }
     return this.cleanTimeInDays;
 
   }
 
   cleanTimeTag() {
+
+    console.group('cleanTimeTag');
+    console.log('todayDay : ', this.todayDay);
+    console.log('cleanDay : ', this.cleanDay);
+    console.log('todayMonth : ', this.todayMonth);
+    console.log('cleanMonth : ', this.cleanMonth);
+    console.log('todayYear : ', this.todayYear);
+    console.log('cleanYear : ', this.cleanYear);
+    console.groupEnd();
+
     // One day
     if (this.cleanTimeInDays === 1) {
       this.tag = 'One day clean';
@@ -104,7 +116,7 @@ export class DatetimePage implements OnInit {
       // 1 year
     } else if ((this.todayDay === this.cleanDay) &&
       (this.todayMonth === this.cleanMonth) &&
-      ((this.todayYear - this.cleanYear) === 1)) {
+      ((this.todayYear - 1) === this.cleanYear)) {
       this.tag = '1 Year clean';
       this.keytagImage = './assets/keytags/1-year.png';
 
@@ -115,23 +127,17 @@ export class DatetimePage implements OnInit {
       this.keytagImage = './assets/keytags/18-months.png';
 
       // Multiple years
-    } else
-      if ((this.todayDay === this.cleanDay) && (this.todayMonth === this.cleanMonth) && (this.cleanYear !== this.todayYear) && ((this.todayYear - this.cleanYear) > 1)) {
-        console.log('*****In CleanTimeTag******');
-        console.log('cleanTimeInDays : ', this.cleanTimeInDays);
-        console.log('todayDay        : ', this.todayDay);
-        console.log('todayMonth      : ', this.todayMonth);
-        console.log('todayYear       : ', this.todayYear);
-        console.log('cleanDay        : ', this.cleanDay);
-        console.log('cleanMonth      : ', this.cleanMonth);
-        console.log('cleanYear       : ', this.todayYear);
-        this.tag = this.cleanTimeInYears + ' years clean';
-        this.keytagImage = './assets/keytags/x-years.png';
+    } else if ((this.todayDay === this.cleanDay) &&
+      (this.todayMonth === this.cleanMonth) &&
+      (this.cleanYear !== this.todayYear) &&
+      ((this.todayYear - this.cleanYear) > 1)) {
+      this.tag = this.cleanTimeInYears + ' years clean';
+      this.keytagImage = './assets/keytags/x-years.png';
 
-      } else {
-        // Not a clean time anniversary today
-        this.tag = 'none';
-      }
+    } else {
+      // Not a clean time anniversary today
+      this.tag = 'none';
+    }
     return this.tag;
   }
 
