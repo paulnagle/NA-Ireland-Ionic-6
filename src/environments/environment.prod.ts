ed99634de058fc0237ae613ba35339766a8e04a0
Paul Nagle <paulnagle@gmail.com>
2019-10-28 12:38:26 +0000

    More translations for cleantime calc page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   oneSignalAndroidSenderId: '297965710473',
   speakersApiUrl: 'https://android.nasouth.ie/conventions.json',
   irelandBMLT: 'https://bmlt.nasouth.ie/main_server/client_interface/json/',
-  tomatoBMLT: 'https://tomato.na-bmlt.org/main_server/client_interface/json/',
+  tomatoBMLT: 'https://tomato.bmltenabled.org/main_server/client_interface/json/',
   getApiUrlServiceGroups: 'https://bmlt.nasouth.ie/main_server/client_interface/json/?switcher=GetServiceBodies',
   wordpressApiUrl: 'https://www.na-ireland.org/wp-json/wp/v2/posts?categories=9'
 };
