c2266b74ec6f96fe512b6699fa88d4937b720665
Paul Nagle <paulnagle@gmail.com>
2024-09-29 11:37:00 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ie.nasouth.apple',
+  appName: 'NA-Ireland-Ionic-6',
+  webDir: 'www'
+};
+
+export default config;
